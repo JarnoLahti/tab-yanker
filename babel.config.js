1ac796426b8de63e727df76c322d7d60d99a9b67
Jarno <jarno_lahti@icloud.com>
2026-03-26 14:47:30 +0200

    env based build

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = function (api) {
+  api.cache(true);
+  const plugins = [];
+  if (process.env.NODE_ENV === 'production') {
+    plugins.push('babel-plugin-transform-remove-console');
+  }
+  return {
+    presets: [
+      '@babel/preset-typescript'
+    ],
+    plugins
+  };
+};
